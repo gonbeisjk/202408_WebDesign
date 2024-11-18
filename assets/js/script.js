d6d746b62b76994b65b2fc56fdc73c4886193b8c
taro shinjyuku <tarosjk@gmail.com>
2024-11-18 10:28:03 +0900

    キービジュアルのスライダーの完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,31 @@ $(function () {
     $('body').toggleClass('is-openMenu');
   })
 })
+
+const xyz = 'あいうえお';
+
+// 即時関数
+(function () {
+  // Swiperの設定
+  const kv = new Swiper('#kv', {
+    effect: "fade",
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //   delay: 3000,
+    // },
+  })
+
+  // document.querySelector('#kv-btn')
+  //   .addEventListener('click', () => {
+  //     kv.slideNext()
+  //   })
+
+})()
+
+
+function test(a) { } //関数定義
+test('あいうえお') //関数呼び出し
